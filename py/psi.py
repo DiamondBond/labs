3238ee7d1592774838de1a0e4baa80faf7842cb4
diamondbond <diamondbond1@gmail.com>
2020-06-12 23:18:28 +0530

    psi(x, y)

---
+++ py/psi.py
@@ -0,0 +1,14 @@
+# y = n / 2 * (2 * a + (n - 1) * d)
+# a = ((2 * y) / n - (n - 1) * x) / 2
+
+ans = []
+x = 13
+y = 1000
+
+# main
+for n in range(1,10000):
+    a = ((2 * y) / n - (n - 1) * x) / 2
+    if a%1==0:
+        a = int(a)
+        ans.append((a, n))
+        print(ans)
